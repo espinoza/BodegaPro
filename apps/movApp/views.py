20c1691983d12aa3aefa6568cc395529446fccd9
unknown <carlosbravoeii@hotmail.com>
2021-04-25 22:28:38 -0400

    cree primera version de dashboard

--- apps/movApp/views.py
+++ apps/movApp/views.py
@@ -7,9 +7,26 @@ from .models import MovItem, MovEstado, MovEncabezado, Stock
 from django.contrib import messages
 
 def gotoDashboard(request,id_user,tipo):
-    context = {
-        'id_user' : id_user,
-        'tipo' : tipo,
-        'user' : User.objects.get(id = request.session['id'])
-    }
-    return render(request,'dashboard.html',context)
+    if "id" in request.session:
+        all_encabezados=MovEncabezado.objects.all()    
+        all_areas=Area.objects.all()
+        all_movimientos=TipoMov.objects.all()
+        if len(User.objects.filter(id=id_user))>0:
+            this_user= User.objects.get(id=id_user)
+        else:
+            this_user= User.objects.get(id=request.session["id"])
+        mis_movimientos=[]
+        for estado_mov in this_user.movs_asociados.all():
+            mis_movimientos.append(estado_mov.mov_encabezado)
+        print(mis_movimientos)
+        context = {
+            'id_user' : id_user,
+            'tipo' : tipo,
+            'user' : User.objects.get(id = request.session['id']),
+            'all_encabezados':all_encabezados,
+            'all_areas':all_areas,
+            'all_movimientos':all_movimientos,
+            'mis_movimientos':mis_movimientos
+        }
+        return render(request,'dashboard.html',context)
+    return redirect("/")
