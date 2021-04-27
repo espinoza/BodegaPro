f19b8dd5acae8a71984737394b1c511ea194e498
unknown <carlosbravoeii@hotmail.com>
2021-04-26 23:12:56 -0400

    filtros is active y links en folios

--- apps/movApp/views.py
+++ apps/movApp/views.py
@@ -11,8 +11,8 @@ from django.contrib import messages
 def gotoDashboard(request,id_user,tipo):
     if "id" in request.session:
         all_encabezados=MovEncabezado.objects.all()    
-        all_areas=Area.objects.all()
-        all_movimientos=TipoMov.objects.all()
+        all_areas=Area.objects.filter(is_active = True).order_by('pos')
+        all_movimientos=TipoMov.objects.filter(is_active = True).order_by('pos')
         if len(User.objects.filter(id=id_user))>0:
             this_user= User.objects.get(id=id_user)
         else:
@@ -28,7 +28,7 @@ def gotoDashboard(request,id_user,tipo):
             'all_encabezados':all_encabezados,
             'all_areas':all_areas,
             'all_movimientos':all_movimientos,
-            'mis_movimientos':mis_movimientos
+            'mis_movimientos':mis_movimientos,            
         }
         return render(request,'dashboard.html',context)
     return redirect("/")
