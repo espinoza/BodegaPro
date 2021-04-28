c1def9e5973c83e07c944b2ebf00f6e7848ae981
Felipe-Infante <finfante@mqs.cl>
2021-04-28 04:51:40 -0400

    Se agrega algo de diseño y funcionalidad a movimientos. Se deja forms para editar cantidad y eliminar. Se deja form para pasar de creado a solicitado.

--- apps/movApp/urls.py
+++ apps/movApp/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #rdc/...
-    path('<int:id_user>/<str:tipo>', views.gotoDashboard, name='dashboard'), #id_user: id_user o '0:all' - tipo: 'allmov','activemov'
+    # rdc/...
+    # id_user: id_user o '0:all' - tipo: 'allmov','activemov'
+    path('<int:id_user>/<str:tipo>', views.gotoDashboard, name='dashboard'),
     path('requestNew', views.requestNewMov, name='request_new_mov'),
     path('new', views.createNewMov, name='create_new_mov'),
     path('view/<int:id_mov_encabezado>/', views.gotoMov, name='goto_mov'),
+    path('view/<int:id_mov_encabezado>/eliminarItem',
+         views.eliminarItem, name='delete_item'),
+    path('view/<int:id_mov_encabezado>/editarItem',
+         views.editarItem, name='edit_item'),
+    path('view/<int:id_mov_encabezado>/cambiarEstado',
+         views.cambiarEstado, name='change_status'),
 ]
